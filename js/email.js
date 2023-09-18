7c01b30f95d3c708826619b961e1407bac3e4e07
nagaraja-tf-22 <nagaraja@techfnatic.com>
2023-09-18 23:26:07 +0530

    UI improvements

--- js/email.js
+++ js/email.js
@@ -7,6 +7,9 @@ const btn = document.getElementById('submitButton');
 
 document.getElementById('myForm').addEventListener('submit', function(event) {
     event.preventDefault();
+    const rootStyles = getComputedStyle(document.documentElement);
+    const mainColor = rootStyles.getPropertyValue('--blue');
+
     
    btn.value = 'Sending...';
    var params={
@@ -15,7 +18,6 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
         from_email: document.getElementById('from_email').value,
         from_subject: document.getElementById('from_subject').value,
         from_msg: document.getElementById('from_msg').value,
-        from_name: document.getElementById('from_name').value,
         reply_to:"nagaraj.nani.business@gmail.com",
     };
     emailjs.send(SERVICE_ID, TEMPLATE_ID,params).then(
@@ -24,12 +26,15 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
             btn.value = 'Send';
             btn.setAttribute('disabled', true);
             document.getElementById('email-response').innerHTML='Thank you for your message.'
+            document.getElementById('email-response').style.color=mainColor;
+            document.getElementById('myForm').reset();
         }
     ).catch(
         error => {
         console.log("failed");
         btn.value = 'Send';
-        document.getElementById('email-response').innerHTML='Unkown technical issue occured.'
+        document.getElementById('email-response').innerHTML='A technical issue occured.'
+        document.getElementById('email-response').style.color=mainColor;
         }
     )
 
